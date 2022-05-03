e8f0220c68b7db38d3e54e9c6bd4f4cb59c6cf9d
bruno <barcanjocs@outlook.com>
2022-05-03 16:59:02 +0100

    more work on downloading and processing files. Also changes to the landsat library to make it work with older scenes#

--- remote_sensing/data_utils.py
+++ remote_sensing/data_utils.py
@@ -12,8 +12,7 @@ from osgeo import gdal
 # TODO: DESCRIBE WHERE THE FILES SHOULD BE :
 #   - kml file
 #   - locations (path/row) file
-
-def download_region_data(region, user, passwd, location_file_path = "location_data.json") :
+def download_region_data(region, user, passwd, root_data_dir = "./data", location_file_path = "location_data.json") :
     """
     Downloads tiff files from earthexplorer for a single region (given by region name)
     The resulting folder structure is one image per row/path combination per year-month folder
@@ -21,19 +20,20 @@ def download_region_data(region, user, passwd, location_file_path = "location_da
     Each year-month contains the tiffs to reconstruct the entire region for that time-period
     """
 
+    # Initialize API
     username = user
     password = passwd
     api = API(username, password)
     ee = EarthExplorer(username, password)
-    import shutil
+
 
     with open(location_file_path) as location_file :
         locations = json.load(location_file)
-        regions = list(locations.keys())
         
         # Get the coordinates of the defining polygon to do scene search
-        with open("data/" + region + "/" + region + ".kml") as kml_file :
+        with open(root_data_dir + region + "/" + region + ".kml") as kml_file :
             for line in kml_file.readlines() :
+                # Get all the edges of the polygon of the region
                 if "<Polygon>" in line:
                     line = line.replace("<Polygon><outerBoundaryIs><LinearRing><coordinates>", "")
                     line = line.replace("</coordinates></LinearRing></outerBoundaryIs></Polygon>", "")
@@ -42,7 +42,7 @@ def download_region_data(region, user, passwd, location_file_path = "location_da
 
         all_scenes = []
             
-            # Get all scenes touching the polygon
+        # Get all scenes touching the polygon
         for coordinate_pair in coordinate_pairs:
             longi = float(coordinate_pair[0])
             lati = float(coordinate_pair[1])
@@ -53,9 +53,11 @@ def download_region_data(region, user, passwd, location_file_path = "location_da
                 max_results=50000
             )
 
+            #print(scenes_search[0])
+
             all_scenes.extend(scenes_search)
 
-        # Get the path/row pairs for the scenes that compose the region entirely
+        # Filter the scenes for only the relevant path/row pairs
         relevant_scenes = []
         path_row_pairs = locations[region]["path_row_pairs"]
         for path_row_pair in path_row_pairs :
@@ -67,6 +69,8 @@ def download_region_data(region, user, passwd, location_file_path = "location_da
                     if scene not in relevant_scenes :
                         relevant_scenes.append(scene)
 
+        # Only get one path/row scene per month per year
+        # To be merged into a single one per month
         collected = []
         for scene in relevant_scenes:
             date = scene["acquisition_date"].date()
@@ -76,35 +80,49 @@ def download_region_data(region, user, passwd, location_file_path = "location_da
             info = str(date.year) + "_" + str(date.month) + "_" + str(path) + "_" + str(row)
 
             if info not in collected :
-                path = "./data/" + region + "/" + str(date.year) + "_" + str(date.month)+ "/"
+                path = root_data_dir + region + "/" + str(date.year) + "_" + str(date.month)+ "/"
                 Path(path).mkdir(exist_ok=True)
-                ee.download(scene["entity_id"], path)
+                ee.download(scene["display_id"], path)
                 collected.append(info)
 
+    api.logout()
+    ee.logout()
 
-def process_year_month_folder(region, year, month, delete_after = False) :
+
+def process_region_folder(region, root_data_dir = "./data") :
+    pass
+
+
+def process_year_month_folder(region, year, month,  folder_path, delete = False) :
     """
     1st - creates raster stacks from the downloaded data in the folders (after unzipping)
     2nd - crops all the raster stacks in the folder, according to the corresponding KML file
     3rd - it joins the resulting rasters into a single one to reconstruct the entire region
     """
 
-    path = "./data/" + str(region) + str(year) + "_" + str(month) + "/"
-
    
-    Path(path + "/clipped/").mkdir(exist_ok=True)
+    Path(folder_path + "/clipped/").mkdir(exist_ok=True)
 
-    create_raster_stack(path, region)
+    # creating raster stacks
+    create_raster_stack(folder_path, region)
 
-    for (dirpath, dirnames, filenames) in os.walk(path + "raster_stack") :
+    for (dirpath, dirnames, filenames) in os.walk(folder_path + "raster_stack") :
         for stack in filenames:
-            OutTile = gdal.Warp(path  + "/clipped/" + stack, 
-                        path + "raster_stack/" + stack, 
+            OutTile = gdal.Warp(folder_path  + "/clipped/" + stack, 
+                        folder_path + "raster_stack/" + stack, 
                         cutlineDSName="./data/" + region + "/" + region + '.kml',
                         cropToCutline=True,
                         dstNodata = 0)
 
             OutTile = None
+    
+    
+    for (dirpath, dirnames, filenames) in os.walk(folder_path + "/clipped/") :
+        output_path = folder_path + "/" + year + "_" + month + ".tif"
+        g = gdal.Warp(output_path, filenames, format="GTiff",
+              options=["COMPRESS=LZW", "TILED=YES"]) # if you want
+        g = None
 
-    if delete_after :
-        shutil.rmtree(path + "raster_stack/")
+    # delete warped rasters
+    if delete :
+        shutil.rmtree(folder_path + "raster_stack/")
