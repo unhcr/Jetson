db0511d3d7908f79dc4c833194668f1c7043dd34
Katherine <khof312@gmail.com>
2022-07-11 11:59:45 -0400

    Update configs template for SQL and new training dates.

--- experiment_2/jetson_configs_template.py
+++ experiment_2/jetson_configs_template.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Credentials for postgres database
+sql_host = 'localhost:XXXX' # Replace XXXX with the port on which the server runs 
+sql_user = # YOUR USERNAME
+sql_password = # YOUR PASSWORD
+
 
 # Credentials for FSNAU site
 fsnau_user = # YOUR USERNAME
@@ -10,9 +15,14 @@ ew_ea_user = # YOUR USERNAME
 ew_ea_pass = # YOUR USERNAME
 
 
+
 # Current date 
-current_month    = "2019-11"
-train_test_split = '2018-12-01' # <- the last day in the training data
+current_month    = "2020-08"
+train_test_split = "2018-12-01" # <- the last day in the training data
+
+# Whether to use public or private PRMN
+use_public_prmn = True
+prmn_path = # Local filepath of the private prmn
 
 # Experiment configs
 
